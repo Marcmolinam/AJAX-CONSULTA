b264c4a90b96355a46a5b2538015c332ef487bb5
cetecom <cetecom@LCV10411>
2019-05-17 15:54:35 -0400

    AJAX

---
+++ cargadatos.php
@@ -0,0 +1,12 @@
+<h1>Carga Datos</h1>
+<?php
+
+echo "<h2>Nombre: ".$_POST["nombre"]."</h2>";
+?>
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." class="card-img-top" alt="CARD image cap">
+  <div class="card-body">
+    <h5 class="card-title">Nombre</h5>
+    <p class="card-text"><?=$_POST["nombre"]?></p>
+  </div>
+</div>
